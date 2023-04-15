0fe69d552aa487d6d0abd5be2a7134ecd8f7a986
symonk <jackofspaces@gmail.com>
2023-04-15 18:09:03 +0100

    additional service starting plugin code

--- src/pytest_containers/plugin.py
+++ src/pytest_containers/plugin.py
@@ -1,4 +1,5 @@
 import pytest
+import subprocess
 
 
 @pytest.hookimpl
@@ -11,3 +12,18 @@ def pytest_addoption(parser: pytest.Parser):
         help="The path to the `docker-compose.yml` file responsible for the services that should be started/stopped. "
         "Defaults to the current working directory.",
     )
+
+    
+def start_services(a: int, b: str) -> int:
+    """
+    The function takes an integer and a string as input, converts the string to an integer and returns
+    the sum of the two inputs.
+    
+    :param a: The parameter 'a' is an integer
+    :type a: int
+    :param b: The parameter "b" is a string type parameter
+    :type b: str
+    :return: The function `start_services` is returning the sum of the integer value of the second
+    argument `b` and the first argument `a`. The return type is an integer.
+    """
+    return a + int(b)
