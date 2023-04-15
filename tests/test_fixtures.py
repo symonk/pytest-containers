5f08adde05c5267063289a40c144223fd15361bf
symonk <jackofspaces@gmail.com>
2023-04-15 18:31:46 +0100

    new fixtures and utilities for compose file(s)

---
+++ tests/test_fixtures.py
@@ -0,0 +1,8 @@
+import pytest
+
+def test_docker_compose_flag_is_required(pytester: pytest.Pytester) -> None:
+    ...
+
+    
+def test_docker_compose_path_is_a_path(pytester: Pytest.Pytester) -> None:
+    ...
