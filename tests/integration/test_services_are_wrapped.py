c07efcee35b2874cfa542d83792222d6282988ca
symonk <jackofspaces@gmail.com>
2023-08-07 19:26:22 +0100

    new `project_name` fixture

---
+++ tests/integration/test_services_are_wrapped.py
@@ -0,0 +1,14 @@
+
+
+def test_all_services_are_started() -> None:
+    ...
+    
+    
+def test_all_services_get_torn_down() -> None:
+    ...
+    
+    
+def test_multiple_compose_files_work() -> None:
+    ...
+    
+    
--- tests/test_services_are_started.py
+++
@@ -1,9 +0,0 @@
-def test_httpbin_is_available(pytester):
-    pytester.makepyfile(
-        """
-    def test_services_are_running():
-        assert True
-    """,
-    )
-    result = pytester.runpytest()
-    result.assert_outcomes(passed=1)
